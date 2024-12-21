ad3d45f8fcafc35a077967142e011837accb105d
Araj Ali <arajali979@gmail.com>
2024-12-21 09:18:55 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+/***************************************************
+==================== JS INDEX ======================
+****************************************************
+
+****************************************************/
+
+(function ($) {
+   "use strict";
+    
+   // $('button').click(function(){
+   //    $('.text').hide();
+   // });
+
+   // $('button').click(function(){
+   //    $('*').hide();
+   // });
+
+   // $('button').click(function(){
+   //    $('button').hide();
+   // });
+
+   // $('button').click(function(){
+   //    $('p.text').hide();
+   // });
+
+   // $('button').click(function(){
+   //    $('p:first').hide();
+   // });
+
+   // $('button').click(function(){
+   //    $('ul li:first').hide();
+   // });
+
+   // $('button').click(function(){
+   //    $('ul li:nth-child(2)').hide();
+   // });
+
+   $('button').click(function() {
+      $('a[target="_blank"]').hide();
+   });
+   
+})(jQuery);
