062493058a6725ab200ee7e1afa2ba8691ad8291
Araj Ali <arajali979@gmail.com>
2024-12-23 23:44:42 +0600

    hide show

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,8 +39,33 @@
    //    $('a[target="_blank"]').hide();
    // });
 
-   $("p").dblclick(function() {
-      $(this).hide();
+   // $("p").dblclick(function() {
+   //    $(this).hide();
+   // });
+
+   // $("#hide").click(function(){
+   //    $("p").hide('slow');
+   // });
+   // $("#show").click(function(){
+   //    $("p").show('slow');
+   // });
+
+   // $("#hide").click(function(){
+   //    $("p").hide('fast');
+   // });
+   // $("#show").click(function(){
+   //    $("p").show('fast');
+   // });
+
+   // $("#hide").click(function(){
+   //    $("p").hide(5000);
+   // });
+   // $("#show").click(function(){
+   //    $("p").show(5000);
+   // });
+
+   $("button").click(function(){
+      $("p").toggle();
    });
    
 })(jQuery);
